c1d6f97c88443e02cd9cba17facf24fb3220121b
leomcelroy <leomcelroy@gmail.com>
2021-11-10 13:48:06 -0500

    init

---
+++ examples.js
@@ -0,0 +1,370 @@
+// SET-UP
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const w = canvas.width;
+const h = canvas.height;
+
+// DEBLUR (ANTI-ALIASING)
+ctx.translate(0.5, 0.5);
+
+// DRAW BACKGROUND
+function draw() {
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 0, w, h);
+}
+
+// DRAW CIRCLE
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.beginPath();
+  ctx.arc(156, 250, 50, 0, Math.PI*2);
+  ctx.stroke();
+
+}
+
+// DRAW RECTANGLE
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.beginPath();
+  ctx.rect(20, 20, 150, 100);
+  ctx.stroke();
+
+}
+
+// DRAW LINE
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.beginPath();
+  ctx.moveTo(30, 30);
+  ctx.lineTo(50,70);
+  ctx.stroke();
+}
+
+// FILL LINES
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.beginPath();
+  ctx.moveTo(30, 30);
+  ctx.lineTo(50,70);
+  ctx.lineTo(109,57);
+  ctx.closePath()
+  ctx.stroke();
+  ctx.fillStyle = "red";
+  ctx.fill();
+}
+
+// STYLE LINES
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 4;
+  ctx.beginPath();
+  ctx.moveTo(30, 30);
+  ctx.lineTo(50,70);
+  ctx.closePath()
+  ctx.stroke();
+
+}
+
+// COLORED CIRCLE
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.beginPath();
+  ctx.arc(156, 250, 50, 0, Math.PI*2);
+  ctx.stroke();
+  ctx.fillStyle = "red";
+  ctx.fill();
+
+}
+
+// MOVING CIRCLE
+let x = 0;
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.beginPath();
+  ctx.arc(x, 250, 50, 0, Math.PI*2);
+  ctx.stroke();
+
+  x++;
+}
+
+// WRAP AROUND CIRCLE
+let x = 0;
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.beginPath();
+  ctx.arc(x, 250, 50, 0, Math.PI*2);
+  ctx.stroke();
+
+  x = (x + 1)%w;
+}
+
+// BOUNCING CIRCLE
+let x = 0;
+let velocity_x = 1;
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.beginPath();
+  ctx.arc(x, 250, 50, 0, Math.PI*2);
+  ctx.stroke();
+
+  if (x > w) velocity_x = -1;
+  if (x < 0) velocity_x = 1;
+
+  x += velocity_x;
+}
+
+// CHANGING COLOR CIRCLE
+let h = 0;
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.beginPath();
+  ctx.arc(250, 250, 50, 0, Math.PI*2);
+  ctx.stroke();
+  ctx.fillStyle = `hsl(${h%360}, 100%, 50%)`;
+  ctx.fill();
+
+  h++;
+}
+
+// OSCILLATING
+let step = 0;
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+  const scale = 50;
+  const rate = 1/50;
+  const size = Math.abs(Math.sin(step*rate))*scale;
+  ctx.beginPath();
+  ctx.arc(250, 250, size, 0, Math.PI*2);
+  ctx.stroke();
+
+  step++;
+}
+
+// REPEATS, LOOPS
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+
+  for (let i = 0; i < 5; i++) {
+    ctx.beginPath()
+    ctx.arc(i*45+80, 250, 17, 0, Math.PI*2)
+    ctx.stroke()
+  }
+
+}
+
+// RANDOMNESS
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.beginPath();
+  ctx.arc(100, 250, Math.random()*3+15, 0, Math.PI*2);
+  ctx.stroke();
+}
+
+// NOISE (PERLIN)
+
+
+// EVENT KEYDOWN
+let x = 0;
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+  ctx.beginPath();
+  ctx.arc(x, 250, 40, 0, Math.PI*2);
+  ctx.stroke();
+}
+
+window.addEventListener("keydown", e => {
+  const key = e.key;
+  if (key === "ArrowRight") x += 10;
+  if (key === "ArrowLeft") x -= 10;
+})
+
+// EVENT MOUSEMOVE
+let x = 0;
+let y = 0;
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+  ctx.beginPath();
+  ctx.arc(x, y, 40, 0, Math.PI*2);
+  ctx.stroke();
+}
+
+window.addEventListener("mousemove", e => {
+  x = e.clientX;
+  y = e.clientY;
+})
+
+// EVENT MOUSEDOWN
+let circles = [];
+
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+  for (let i = 0; i < circles.length; i++) {
+    let c = circles[i];
+    ctx.beginPath();
+    ctx.arc(c.x, c.y, 40, 0, Math.PI*2);
+    ctx.stroke();
+  }
+}
+
+window.addEventListener("mousedown", e => {
+  circles.push({ x: e.clientX, y: e.clientY })
+})
+
+// COLLISION
+
+function checkCollided(r1, r2) {
+  return r1.x < r2.x + r2.width &&
+         r1.x + r1.width > r2.x &&
+         r1.y < r2.y + r2.height &&
+         r1.height + r1.y > r2.y;
+}
+
+function drawRect(r) {
+  ctx.beginPath();
+  ctx.rect(r.x, r.y, r.width, r.height);
+  ctx.fillStyle = "blue";
+  ctx.fill();
+}
+
+const rect1 = { x: 20, y: 20, width: 30, height: 30 };
+const rect2 = { x: 100, y: 20, width: 30, height: 30 };
+
+let hasCollided = false;
+
+function draw() {
+  if (!hasCollided) {
+    ctx.fillStyle = "white";
+  } else {
+    ctx.fillStyle = "red";
+  }
+
+  ctx.fillRect(0, 0, w, h);
+
+  drawRect(rect1);
+  drawRect(rect2);
+
+  hasCollided = checkCollided(rect1, rect2);
+}
+
+window.addEventListener("mousemove", e => {
+  rect1.x = e.clientX;
+  rect1.y = e.clientY;
+})
+
+// TRANSLATE
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+
+  ctx.save();
+  ctx.translate(182, 97)
+  ctx.strokeRect(0, 0, 105, 50);
+  ctx.restore();
+}
+
+// ROTATE
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+  
+  ctx.save();
+  ctx.translate(150, 300);
+  ctx.rotate(45);
+  const rectW = 100;
+  const rectH = 50;
+  ctx.strokeRect(-rectW/2, -rectH/2, rectW, rectH);
+  ctx.restore();
+}
+
+// ANIMATED SCALE, ROTATE
+let step = 0;
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+  
+  ctx.save();
+  ctx.translate(158, 189);
+  ctx.rotate(step/100);
+  const rectW = step/3;
+  const rectH = step/3;
+  ctx.strokeRect(-rectW/2, -rectH/2, rectW, rectH);
+  ctx.restore();
+
+  step++;
+}
+
+// OSCILLATING RECTANGLE
+let step = 0;
+function draw() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+  
+  ctx.save();
+  ctx.translate(158, 189);
+  ctx.rotate(step/100);
+  const rectW = Math.sin(step/10)*13+100/3;
+  const rectH = 40;
+  ctx.strokeRect(-rectW/2, -rectH/2, rectW, rectH);
+  ctx.restore();
+
+  step++;
+}
+
+// ROTATING ACCUMULATION
+
+// ctx.globalCompositeOperation = "destination-under"
+let x = w/2;
+let y = h/2;
+let step = 0;
+function draw() {
+  ctx.fillStyle = "white";
+  // ctx.fillRect(0, 0, w, h);
+  
+  ctx.save();
+  ctx.translate(x, y);
+  ctx.rotate(step/13);
+  const rectW = Math.sin(step/30)*76 + 105;
+  const rectH = Math.sin(step/30)*57 + 66;
+  ctx.beginPath();
+  ctx.rect(-rectW/2, -rectH/2, rectW, rectH);
+  ctx.stroke();
+  ctx.fillStyle=`hsl(${step%360}, 100%, 50%)`;
+  ctx.fill();
+  ctx.restore();
+
+  step++;
+}
+
+// SCALE distorts image, these transformations are better to do with vectors
+
+
+
+
